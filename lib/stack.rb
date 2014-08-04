33d6f58e988b0c7b5a09e17f07a7540fdff49c69
Dan Sharp <Dan.Sharp@irco.com>
2014-08-04 13:15:19 -0600

    REFACTOR: test five: changed from multiple variables to @elements as an Array

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,25 +1,19 @@
 class Stack
   def initialize
-    @element = nil
-    @prev_element = nil
-    @prev_prev_element = nil
+    @elements = []
   end
 
   def IsEmpty
-    @element.nil?
+    @elements.empty?
   end
 
   def Push(element)
-    @prev_prev_element = @prev_element
-    @prev_element = @element
-    @element = element
+    @elements << element
   end
 
   def Pop
-    top = @element
-    @element = @prev_element
-    @prev_element = @prev_prev_element
-    @prev_prev_element = nil
+    top = @elements[-1]
+    @elements = @elements[0..-2]
     top
   end
 
