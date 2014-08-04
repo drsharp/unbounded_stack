2f6734eee14c627be67524498e25a9d59a5ffd7d
Dan Sharp <Dan.Sharp@irco.com>
2014-08-04 12:29:59 -0600

    REFACTOR: pulled stack creation into let() method to DRY specs

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,14 +1,13 @@
 require 'stack'
 
 describe "Stack" do
+  let(:stack) { Stack.new }
 
   it "creates a Stack and verifies that IsEmpty is true." do
-    stack = Stack.new
     expect(stack.IsEmpty).to be(true)
   end
 
   it "Pushes a single object on the Stack and verifies that IsEmpty is false" do
-    stack = Stack.new
     stack.Push("first element")
     expect(stack.IsEmpty).to be(false)
   end
