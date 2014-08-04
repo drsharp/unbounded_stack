d502485da348e42f19e8d27e138f31687e03371a
Dan Sharp <Dan.Sharp@irco.com>
2014-08-04 14:37:11 -0600

    REFACTOR: test 11: Pop() now calls Top()

--- lib/stack.rb
+++ lib/stack.rb
@@ -12,8 +12,7 @@ class Stack
   end
 
   def Pop
-    raise "Cannot Pop an empty Stack" if self.IsEmpty
-    top = @elements[-1]
+    top = self.Top
     @elements = @elements[0..-2]
     top
   end
