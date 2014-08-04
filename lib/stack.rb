d941150b2bb18b7e76be6a3b2502b7699b7e6b4b
Dan Sharp <Dan.Sharp@irco.com>
2014-08-04 14:53:01 -0600

    REFACTOR: removed Pop()

--- lib/stack.rb
+++ lib/stack.rb
@@ -17,12 +17,6 @@ class Stack
     top_element
   end
 
-  def Pop
-    top_element = top
-    @elements = @elements[0..-2]
-    top_element
-  end
-
   def top
     raise "Cannot Top an empty Stack" if self.IsEmpty
     @elements.last
