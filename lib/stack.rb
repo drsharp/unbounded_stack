470fe0b2322ef1e14dec214b5517b2b1dae99131
Dan Sharp <Dan.Sharp@irco.com>
2014-08-04 12:57:31 -0600

    REFACTOR: test four: removed @is_empty variable and just using @element variable

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,17 @@
 class Stack
   def initialize
-    @is_empty = true
+    @element = nil
   end
 
   def IsEmpty
-    @is_empty
+    @element.nil?
   end
 
   def Push(element)
     @element = element
-    @is_empty = false
   end
 
   def Pop
-    @is_empty = true
     top = @element
     @element = nil
     top
