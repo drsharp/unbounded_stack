e4e99c167b900c6bd59eaaaf2e04e7bb5889fd9f
Jason Robb <jlrobb@irco.com>
2014-12-02 10:24:01 -0700

    rename list_of_stack_elements to just stack

--- lib/unbounded_stack.rb
+++ lib/unbounded_stack.rb
@@ -2,27 +2,27 @@
 class UnboundedStack
 
   def empty?
-    list_of_stack_elements.empty?
+    stack.empty?
   end
 
   def push(a)
-    list_of_stack_elements.push(a)
+    stack.push(a)
   end
 
   def pop
     raise "Cannot Pop an empty Stack" if empty?
-    list_of_stack_elements.pop
+    stack.pop
   end
 
   def top
     raise "Cannot Top an empty Stack" if empty?
-    list_of_stack_elements.last
+    stack.last
   end
 
   private
 
-  def list_of_stack_elements
-    @list_of_stack_elements ||= Array.new
+  def stack
+    @stack ||= Array.new
   end
 
 end
