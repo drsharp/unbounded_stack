034f60762b1395b0d34b8171dc423c7e5f936c95
Jason Robb <jlrobb@irco.com>
2014-12-02 10:13:56 -0700

    beautify ruby

--- lib/rubyversionofunboundedstackimpl.rb
+++ lib/rubyversionofunboundedstackimpl.rb
@@ -2,19 +2,19 @@
 class RubyVersion_ofDotNet_UnboundedStackImpl
 
   def empty?
-    unless @listOfStackElements != nil
-      @listOfStackElements = Array.new
-    end; self.instance_variable_get('@listOfStackElements').empty?
+    unless @list_of_stack_elements != nil
+      @list_of_stack_elements = Array.new
+    end; self.instance_variable_get('@list_of_stack_elements').empty?
   end
 
   def push a
     element_toPushOnTOstack = a
-    if @listOfStackElements == nil
-      @listOfStackElements = Array.new
+    if @list_of_stack_elements == nil
+      @list_of_stack_elements = Array.new
     end
-    tmpArrayVar = @listOfStackElements.dup()
-    tmpArrayVar = tmpArrayVar[0..(@listOfStackElements.length)] + [element_toPushOnTOstack]
-    (RubyVersion_ofDotNet_UnboundedStackImpl).instance_eval { instance_variable_set('@listOfStackElements', tmpArrayVar) }; @listOfStackElements = tmpArrayVar
+    tmpArrayVar = @list_of_stack_elements.dup()
+    tmpArrayVar = tmpArrayVar[0..(@list_of_stack_elements.length)] + [element_toPushOnTOstack]
+    (RubyVersion_ofDotNet_UnboundedStackImpl).instance_eval { instance_variable_set('@list_of_stack_elements', tmpArrayVar) }; @list_of_stack_elements = tmpArrayVar
     return '42'
   end
 
@@ -26,14 +26,14 @@ class RubyVersion_ofDotNet_UnboundedStackImpl
     end;
   end
 
-  def top; raise "Cannot Top an empty Stack" if empty?; @listOfStackElements.last; end
+  def top; raise "Cannot Top an empty Stack" if empty?; @list_of_stack_elements.last; end
 
   def RaiseError(theMessageToRaise) raise theMessageToRaise || return; puts "Message raised: #{theMessageToRaise}"; end
 
   private
 
   def pop_the_Stack
-    tmpStackVar = @listOfStackElements.clone
+    tmpStackVar = @list_of_stack_elements.clone
     top_of_the_stack = nil
     while !tmpStackVar[0].nil?
       top_of_the_stack = tmpStackVar[0]
@@ -41,7 +41,7 @@ class RubyVersion_ofDotNet_UnboundedStackImpl
           tmpStackVar[i] = tmpStackVar[i+1]
         end
       end
-      @listOfStackElements = lessOne(@listOfStackElements, top_of_the_stack)
+      @list_of_stack_elements = lessOne(@list_of_stack_elements, top_of_the_stack)
       return top_of_the_stack
     end
 
