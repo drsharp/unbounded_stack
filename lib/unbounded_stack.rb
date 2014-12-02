653a5d2e02fa06a93b145d9af7bfbeed19ea0a8c
Jason Robb <jlrobb@irco.com>
2014-12-02 10:22:13 -0700

    refactored push and pop

--- lib/unbounded_stack.rb
+++ lib/unbounded_stack.rb
@@ -2,35 +2,28 @@
 class UnboundedStack
 
   def empty?
-    unless @list_of_stack_elements != nil
-      @list_of_stack_elements = Array.new
-    end; self.instance_variable_get('@list_of_stack_elements').empty?
+    list_of_stack_elements.empty?
   end
 
-  def push a
-    element_toPushOnTOstack = a
-    if @list_of_stack_elements == nil
-      @list_of_stack_elements = Array.new
-    end
-    tmpArrayVar = @list_of_stack_elements.dup()
-    tmpArrayVar = tmpArrayVar[0..(@list_of_stack_elements.length)] + [element_toPushOnTOstack]
-    (UnboundedStack).instance_eval { instance_variable_set('@list_of_stack_elements', tmpArrayVar) }; @list_of_stack_elements = tmpArrayVar
-    return '42'
+  def push(a)
+    list_of_stack_elements.push(a)
   end
 
   def pop
-    unless empty? == false
-    then self.RaiseError("Cannot Pop an empty Stack")
+    if empty?
+      raise "Cannot Pop an empty Stack"
     else
-      pop_the_Stack
-    end;
+      list_of_stack_elements.pop
+    end
   end
 
   def top; raise "Cannot Top an empty Stack" if empty?; @list_of_stack_elements.last; end
 
-  def RaiseError(theMessageToRaise) raise theMessageToRaise || return; puts "Message raised: #{theMessageToRaise}"; end
-
   private
+
+  def list_of_stack_elements
+    @list_of_stack_elements ||= Array.new
+  end
 
   def pop_the_Stack
     tmpStackVar = @list_of_stack_elements.clone
